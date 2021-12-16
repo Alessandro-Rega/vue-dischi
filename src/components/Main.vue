28ec02b5137c1189224b226af74892198dff38c7
Alessandro-Rega <regaale47@gmail.com>
2021-12-16 21:31:12 +0100

    insert main + dischi

--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,12 +1,37 @@
 <template>
   <main>
-
+      <div class="container">
+        <Dischi v-for="(disco, index) in arrayDischi" :key="index" :dati="disco"/>
+      </div>
   </main>
 </template>
 
 <script>
+import axios from 'axios';
+import Dischi from './dischi/Dischi.vue'
+
 export default {
-    name: 'Main'
+    name: 'Main',
+    components:{
+        Dischi
+    },
+    data(){
+        return {
+            arrayDischi: null
+        }
+    },
+    created(){
+        axios.get('https://flynn.boolean.careers/exercises/api/array/music')
+        .then((response) => {
+            // handle success
+            this.arrayDischi = response.data.response;
+            console.log(this.arrayDischi);
+        })
+        .catch(function (error) {
+            // handle error
+            console.log(error);
+        });
+    }
 }
 </script>
 
@@ -16,6 +41,15 @@ main{
     width: 100%;
     height: calc(100vh - 90px);
     background-color: #1e2d3b;
+    padding-top: 50px;
+
+    .container{
+        width: 70%;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        justify-content: space-between;
+    }
 }
 
 </style>
---
+++ src/components/dischi/Dischi.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="disco">
+      <img :src="dati.poster" alt="immagine">
+      <h3 class="titolo">{{dati.title}}</h3>
+      <span class="autore">{{dati.author}}</span>
+      <span class="data">{{dati.year}}</span>
+  </div>
+</template>
+
+<script>
+export default {
+    name: 'Dischi',
+    props:{
+        dati: Object
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+.disco{
+    width: 200px;
+    padding: 15px;
+    background-color: #2e3a46;
+    text-align: center;
+    margin: 0 20px 20px 20px;
+
+    img{
+        width: 100%;
+    }
+
+    .titolo{
+        color: white;
+        font-size: 25px;
+        padding: 10px 0 20px 0;
+    }
+
+    span{
+        color: #7b7e79;
+        font-size: 20px;
+        font-weight: 500;
+    }
+
+    .autore{
+        display: block;
+    }
+}
+
+</style>
