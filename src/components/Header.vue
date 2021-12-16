505d04a7f318bbcaf88a1bd57dd571b6c1efd16e
Alessandro-Rega <regaale47@gmail.com>
2021-12-16 19:50:10 +0100

    insert layout base

---
+++ src/components/Header.vue
@@ -0,0 +1,26 @@
+<template>
+  <header>
+      <Logo/>
+  </header>
+</template>
+
+<script>
+import Logo from './Logo.vue'
+
+export default {
+    name: 'Header',
+    components:{
+        Logo,
+    }
+}
+</script>
+
+<style lang="scss" scoped>
+
+header{
+    width: 100%;
+    height: 90px;
+    background-color: #2e3a46;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,21 @@
+<template>
+  <main>
+
+  </main>
+</template>
+
+<script>
+export default {
+    name: 'Main'
+}
+</script>
+
+<style lang="scss" scoped>
+
+main{
+    width: 100%;
+    height: calc(100vh - 90px);
+    background-color: #1e2d3b;
+}
+
+</style>
